1fa6a06786b5204c8d7bcd33032911a67d20f2f8
N3cro0o <calko.n3cro0o@gmail.com>
2024-06-26 02:19:29 +0200

    init

---
+++ main.rs
@@ -0,0 +1,6 @@
+fn main(){
+    let mut a: i64 = 23; // let - varible declaration, I guess it's const without any modifications; mut - mutable
+    let b: i64 = 123;
+    a += b;
+    println!("{a}"); // "{}" - to use variable as a string
+}
